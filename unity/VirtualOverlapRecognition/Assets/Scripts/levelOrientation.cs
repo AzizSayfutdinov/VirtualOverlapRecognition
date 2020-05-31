c847922cf3945eab4dbac53ea54d90327f6de378
AzizSayfutdinov <aziz.sayfutdinov@gmx.at>
2020-06-01 01:35:42 +0200

    Added function for level 3D Objects horizontally

---
+++ unity/VirtualOverlapRecognition/Assets/Scripts/levelOrientation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class levelOrientation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // print global and local values
+        Debug.Log("Global angle: " + transform.eulerAngles.z);
+        Debug.Log("Local angle: " + transform.localEulerAngles.z);
+
+        if(transform.eulerAngles.z != 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+    }
+}
