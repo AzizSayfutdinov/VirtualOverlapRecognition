4886b7e72cdd79ed7c8bff688656948a86349a98
AzizSayfutdinov <aziz.sayfutdinov@gmx.at>
2020-12-20 16:17:58 +0100

    Added button interaction script - not working yet

---
+++ unity/RobotImageTracking/Assets/Scripts/StartRobotButtonInteraction.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Microsoft.MixedReality.Toolkit;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class GameObjEvent : UnityEvent<GameObject> { }
+
+public class StartRobotButtonInteraction : Button
+{
+
+    public UnityEvent OnMyButtonClicked;
+
+    void Start()
+    {
+        if(OnMyButtonClicked == null)
+        {
+            this.OnMyButtonClicked = new UnityEvent();
+        }
+        OnMyButtonClicked.AddListener(OutputDebug);
+        
+    }
+
+    public void OutputDebug()
+    {
+        Debug.Log("Hello World");
+    }
+
+}
